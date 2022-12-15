e6100ea56fe54a966131e6f7e6d6ddf2c2fc7a26
keerthanna <keerthannav@gmail.com>
2022-12-15 12:35:20 +0530

    nowcommit

--- todostatic/todostatic/settings.py
+++ todostatic/todostatic/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,8 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 STATIC_URL = 'static/'
 STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
