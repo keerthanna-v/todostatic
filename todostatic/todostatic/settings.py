18d36efa04d6a7b49c9cfab4c784624187ca7b9b
keerthanna <keerthannav@gmail.com>
2022-12-15 12:25:40 +0530

    now

--- todostatic/todostatic/settings.py
+++ todostatic/todostatic/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'todostatic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todostaticdb',
+        'USER':'root',
+        'PASSWORD':'test1234'
     }
 }
 WHITENOISE_USE_FINDERS = True
